fe5be37bf3a600931e4a4eda74b0d6ce74f5bb9c
abstract-animation-9914 <vm930@hotmail.com>
2019-02-20 19:48:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
 
+def swap_elements(array)
+  swaped_array = array.clone
+  
+  # array.each do |ele|
+  #   swaped_array.push(ele)
+  # end 
+  
+  swaped_array[2] = array[1]
+  swaped_array[1] = array[2]
+  
+  return swaped_array
+end 
